f1decb86dfdb2aa81f474b5a953676e48f57d989
SerhiiMordvaniuk <sm636332@gmail.com>
2024-10-30 16:36:21 +0000

    Deploying to gh-pages from @ SerhiiMordvaniuk/goit-js-hw-11@071a56fbc22e3d1542503da1cc4a53f979398143 🚀

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const o of r.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&i(o)}).observe(document,{childList:!0,subtree:!0});function s(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function i(e){if(e.ep)return;e.ep=!0;const r=s(e);fetch(e.href,r)}})();
+(function(){const e=document.createElement("link").relList;if(e&&e.supports&&e.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))n(t);new MutationObserver(t=>{for(const r of t)if(r.type==="childList")for(const s of r.addedNodes)s.tagName==="LINK"&&s.rel==="modulepreload"&&n(s)}).observe(document,{childList:!0,subtree:!0});function l(t){const r={};return t.integrity&&(r.integrity=t.integrity),t.referrerPolicy&&(r.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?r.credentials="include":t.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function n(t){if(t.ep)return;t.ep=!0;const r=l(t);fetch(t.href,r)}})();const c=document.querySelector(".form"),a=document.querySelector(".input"),u=document.querySelector(".galleri-list");let i=null;const p="46809908-9f97c0ef37b027eaa1f813844";c.addEventListener("submit",o=>{o.preventDefault(),i=a.value,f(i),c.reset()});function f(o){fetch(`https://pixabay.com/api/?key=${p}&q=${o}`).then(e=>{if(!e.ok)throw new Error("error");return e.json()}).then(e=>{u.innerHTML=d(e.hits)}).catch(e=>console.log(e))}function d(o){return o.map(e=>`
+            <li class="gallery-item">
+            <img class="galerry-img" src="${e.webformatURL}" alt="${e.tags}" width="300"/>
+            <p class="gallery-txt">Likes <span>${e.likes}</span></p>
+            <p>Views<span>${e.views}</span></p>
+            <p>Comments<span>${e.comments}</span></p>
+            <p>Downloads<span>${e.downloads}</span></p>
+            </li>
+        `).join("")}
 //# sourceMappingURL=index.js.map
