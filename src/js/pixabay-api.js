77704b5442d1261ec1534fcc2562adc2d6a710c6
Serhii Mordvaniuk <sm636332@gmail.com>
2024-11-01 17:08:23 +0100

    done

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,47 @@
+"use strict"
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+import { createGallery } from './render-function'
+
+const API_KEY = "46809908-9f97c0ef37b027eaa1f813844";
+const loader = document.querySelector('.loader'); 
+const gallery = document.querySelector(".gallery-list")
+
+
+export function searchImage(q) {
+    loader.classList.remove('loader-unvisible');
+    gallery.innerHTML = "";
+    fetch(`https://pixabay.com/api/?key=${API_KEY}&q=${q}`)
+        .then(response => {
+            loader.classList.add('loader-unvisible');
+            if (!response.ok) {
+            throw new Error("error")
+            }
+            else {
+                return response.json()
+            }
+        })
+        .then(data => {
+            if (data.hits.length == 0) {
+                iziToast.error({
+                message: 'Sorry, there are no images matching your search query. Please try again!',
+                position: 'center',
+                color: '#ca0000',
+                messageColor: "white",
+                close: true,
+                timeout: 2000,
+                progressBar: true,
+                iconColor: "white",
+                icon: false,
+                maxWidth: "300"
+            });
+            }
+            gallery.innerHTML = createGallery(data.hits)
+            let lightbox = new SimpleLightbox('.gallery-item a');
+        })
+        .catch(error => console.log(error))
+    }
--- src/main.js
+++ src/main.js
@@ -2,17 +2,11 @@
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
-
-
+import { searchImage } from './js/pixabay-api'
 
 const form = document.querySelector(".form");
 const input = document.querySelector(".input");
-const gallery = document.querySelector(".galleri-list")
 let search = null;
-const API_KEY = "46809908-9f97c0ef37b027eaa1f813844";
-const loader = document.querySelector('.loader'); 
 
 form.addEventListener("submit", (event) => {
     event.preventDefault()
@@ -20,7 +14,7 @@ form.addEventListener("submit", (event) => {
     if (search.trim() === "") {
         iziToast.error({
                 message: 'Enter your query',
-                position: 'topCenter',
+                position: 'center',
                 color: '#ca0000',
                 messageColor: "white",
                 close: false,
@@ -29,74 +23,10 @@ form.addEventListener("submit", (event) => {
                 iconColor: "white",
                 con: false,
             });
-
     }
     else {
         searchImage(search)
     }
     form.reset()
 });
-
-
-
-function searchImage(q) {
-    fetch(`https://pixabay.com/api/?key=${API_KEY}&q=${q}`)
-        .then(response => {
-            loader.classList.add('show');
-
-            if (!response.ok) {
-            throw new Error("error")
-            }
-            return response.json()
-        })
-        .then(data => {
-            if (data.hits.length == 0) {
-                iziToast.error({
-                message: 'Sorry, there are no images matching your search query. Please try again!',
-                position: 'center',
-                color: '#ca0000',
-                messageColor: "white",
-                close: true,
-                timeout: 2000,
-                progressBar: true,
-                iconColor: "white",
-                icon: false,
-                maxWidth: "300"
-            });
-            }
-            gallery.innerHTML = createGallery(data.hits)
-        })
-        .catch(error => console.log(error))
-        }
-
-
-function createGallery(arr) {
-    return arr.map(item => 
-        `
-            <li class="gallery-item">
-                <img class="galerry-img" src="${item.webformatURL}" alt="${item.tags}" width="360"/>
-                <div class="galerry-txt">
-                    <div class="item-txt">
-                        <p>Likes</p>
-                        <span class="item-span">${item.likes}</span>
-                    </div>
-                    <div class="item-txt">
-                        <p>Views</p>
-                        <span class="item-span">${item.views}</span>
-                    </div>
-                    <div class="item-txt">
-                        <p>Comments</p>
-                        <span class="item-span">${item.comments}</span>
-                    </div>
-                    <div class="item-txt">
-                        <p>Downloads</p>
-                        <span class="item-span">${item.downloads}</span>
-                    </div>
-                </div>
-            </li>
-        `
-    ).join("")
-}
-
-
 
